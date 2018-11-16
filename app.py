1b1435a5e0cbe5327f77d1334fe34fe390f3610f
Ubuntu <ubuntu@ip-172-31-41-136.us-east-2.compute.internal>
2018-11-16 17:33:55 +0000

    Added Static/Template Folders

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+from flask import render_template
 @app.route('/')
-def hello_world():
-	return 'Hello, World!'
+def hello(name=None):
+    return render_template('fanfix.html', name=name)
+
+@app.route('/informationInput.html/')
+def hello_world(name=None):
+	return render_template('informationInput.html', name=name)
 
 if __name__ == "__main__":
     app.run()
