8998b0e1dc16d27fd3d75a1ff13b45178230075b
Ubuntu <ryan@470Recommender.upnpzrhfbcfujp1cirkwl2sqta.gx.internal.cloudapp.net>
2018-11-20 03:48:01 +0000

    asd

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 import recommend
 import userInfo
 import json_lines
+import pageRankRecommender as PRR
 app = Flask(__name__)
 
 #adding global variables
@@ -37,8 +38,9 @@ def outputPage():
         link = request.form['link']
         #favoriteAuthors = ["/u/1138361/iheartmwpp", "/u/8545331/Professor-Flourish-and-Blotts", "/u/4286546/Missbexiee", "/u/1697963/lydiamaartin", "/u/609412/Crystallic-Rain"]
         favoriteAuthors = userInfo.getFavoriteAuthors(link)
-        result = recommend.recommender(favoriteAuthors)
-
+        #result = recommend.recommender(favoriteAuthors)
+        scores = [ (link, score)for link, score in prRecommender.predict().items()]
+        result = scores[:10]
         print(result[:10])
         return render_template('recommendation.html', data=result[:10])
 	
@@ -54,13 +56,14 @@ def startup():
     global userFavs
     global topStories
     global stories, users
+    global prRecommender
     stories = []
     users = []
     with app.open_resource('result.jl') as f:  
         for line in f:
             j = json.loads(line)
             if j["pageType"] == "user":
-                users.append({'name':j['name'], 'name':j['stories']})
+                users.append({'name':j['name'], 'stories':j['stories']})
                 favAuthors = []
                 favs = j["favorites"]
                 for elem in favs:
@@ -80,6 +83,7 @@ def startup():
                     #if the current top story for the author has less favorites than the new story then make the new story the top story. else don't change anything.
                     if int(topStories[author][1]) < int(favs):
                         topStories[author] = (link, int(favs))   
+    prRecommender = PRR.pageRankRecommender()
     
 startup()
 
